8881963f143f632c0421b314bdadd87becb1329a
Garrett Smith <g@rre.tt>
2017-11-13 23:17:22 -0600

    Pretty good

--- init.el
+++ init.el
@@ -26,9 +26,15 @@
 
 (setq package-list
       '(
+	beacon
+	browse-kill-ring
 	color-theme-sanityinc-tomorrow
+	crux
+	easy-kill
+	git-timemachine
 	magit
 	markdown-mode
+	move-text
 	savehist
 	smart-mode-line
 	undo-tree
@@ -51,12 +57,28 @@
 ;; General config
 ;;===================================================================
 
+(beacon-mode 1)
+(setq beacon-color "#ffff00")
+(browse-kill-ring-default-keybindings)
+(crux-reopen-as-root-mode)
+(global-set-key [remap kill-ring-save] 'easy-kill)
 (global-undo-tree-mode)
 (menu-bar-mode -1)
-(set-default 'truncate-lines t)
 (savehist-mode 1)
+(set-default 'truncate-lines t)
 (smart-mode-line-enable)
 (volatile-highlights-mode t)
+
+;;===================================================================
+;; Close on successful compile
+;;===================================================================
+
+(defun compilation-exit-autoclose (status code msg)
+  (when (and (eq status 'exit) (zerop code))
+    (bury-buffer)
+    (delete-window (get-buffer-window (get-buffer "*compilation*"))))
+  (cons msg code))
+(setq compilation-exit-message-function 'compilation-exit-autoclose)
 
 ;;===================================================================
 ;; File name mode associations
@@ -81,6 +103,12 @@
 (global-set-key (kbd "C-x l") 'ibuffer)
 (global-set-key (kbd "C-x f") 'find-file-at-point)
 (global-set-key (kbd "M-g s") 'magit-status)
+(global-set-key (kbd "M-g t") 'git-timemachine)
+(global-set-key (kbd "ESC <up>") 'move-text-up)
+(global-set-key (kbd "ESC <down>") 'move-text-down)
+(global-set-key (kbd "C-k") 'crux-smart-kill-line)
+(global-set-key (kbd "C-c d") 'crux-delete-file-and-buffer)
+(global-set-key (kbd "C-c r") 'crux-rename-file-and-buffer)
 
 ;;===================================================================
 ;; Other
