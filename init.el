4a08a065d09b1c13071701580ffc3a467cd651c1
Garrett Smith <g@rre.tt>
2017-12-10 08:21:16 -0800

    Changes

--- init.el
+++ init.el
@@ -63,9 +63,11 @@
 ;; Disable backups
 (setq make-backup-files nil)
 
-;; Web mode indents
+;; Indents
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-markup-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+(setq js-indent-level 2)
 
 ;; Compilation window
 (setq compilation-ask-about-save nil)
@@ -130,7 +132,8 @@
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes
    (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))))
+    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+ '(magit-dispatch-arguments nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
